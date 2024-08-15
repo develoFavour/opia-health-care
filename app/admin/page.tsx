a59d9a170e991e66ff3c80287c51b61d9f31efad
develoFavour <opiafavour10@gmail.com>
2024-08-15 14:47:20 +0100

    Final Commit

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -47,7 +47,7 @@ const AdminPage = async () => {
           />
         </section>
 
-        <DataTable columns={columns} data={appointments.documents!} />
+        <DataTable columns={columns} data={appointments?.documents!} />
       </main>
     </div>
   );
